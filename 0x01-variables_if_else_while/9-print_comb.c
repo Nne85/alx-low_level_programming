78702b06002538d5e1eda3076815dad4401340b3
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-10 06:27:08 -0800

    Prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,13 +4,14 @@
 /**
  * main - prints all possible combinations of single-digit numbers
  *
- * Decription: prints all possible combinations of single-digit numbers.
+ * Decription: prints all possible combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int c =0;
-	while (c<10)
+	int c = 0;
+
+	while (c < 10)
 	{
 		putchar(48 + c);
 		if (c != 9)
