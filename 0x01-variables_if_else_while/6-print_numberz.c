4a4f9117e187240855e759e47f80f292ca521e67
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-10 05:51:26 -0800

    Prints all single digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,16 @@
  *
  * Description: prints all single digit numbers of base 10 starting from 0
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+		putchar(num + '0');
 
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
