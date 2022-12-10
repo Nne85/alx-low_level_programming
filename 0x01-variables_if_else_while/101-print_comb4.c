dd5f1e9cbbbcceb3a47b9d257371637f3c7c365f
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-10 07:57:33 -0800

    Prints all possible different combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,32 +11,33 @@ int main(void)
 {
 	int c, d, e;
 
-	c = 58;
-	d = 58;
-	e = 58;
+	c = 48;
+	d = 48;
+	e = 48;
 
-	while (c < 68)
+	while (c < 58)
 	{
 		d = c + 1;
-		while (d < 68)
+		while (d < 58)
 		{
 			e = d + 1;
-			while (e <68)
-				{
+			while (e < 58)
+           		{
 					putchar(c);
 					putchar(d);
 					putchar(e);
 
-					if (c < 65 || d < 66 || e < 67)
+					if (c < 55 || d < 56 || e < 57)
 					{
-					
-						putchar(54);
-						putchar(42);
+						putchar(44);
+						putchar(32);
 					}
 					e++;
 			}
+
 			d++;
-	}
+	       }
+
 		c++;
 	}
 
