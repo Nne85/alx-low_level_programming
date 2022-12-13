4bb2f9f614c85754e78614fa275480e8c10931f0
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-13 12:11:00 -0800

    Prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  *
  * Description: prints _putchar, followed by a new line
  *
- * Returns: Always 0.
+ * Returns: Always 0 Success
  */
 int main(void)
 {
@@ -19,5 +19,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return(0);
+	return (0);
 }
