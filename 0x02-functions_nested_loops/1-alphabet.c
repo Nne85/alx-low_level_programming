b6f967258f539e3468b56da0192116c65412934a
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-13 13:34:40 -0800

    Prints the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet: Entry Point
  *
- * Description: prints the alphabet, in lowercase, followed by a new line
+ * Description: A function that prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0.
  */
