06c9840a12f0d2a4c3e335362220b9c1bea47ddf
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-16 04:03:30 -0800

    Write a function that checks for a digit (0 through 9)

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,10 @@
 
 /**
  * _isupper - a function that checks for uppercase character
+ * @c: input
+ *
  * Description: Checks for uppercase character.
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
