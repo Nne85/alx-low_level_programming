17ff93fb9906a27f246bb1cfe809fac447ee2cf4
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-15 23:24:41 -0800

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -4,6 +4,7 @@
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ * Description: prints the largest of three integers.
  * Return: largest number
  */
 
@@ -11,24 +12,22 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if (a > b && a > c)
 	{
-		if (a > c)
-		{
-			largest = a;
-		}
-		else
-		{
-			largest = c;
-		}
+		largest =a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
 	}
 	else if (b > c)
 	{
 		largest = b;
 	}
-		else
-		{
-			largest = c;
-		}
-		return (largest);
+	else 
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
