a1a54d5146c9201bbb739f95ac0a8f8115484857
Nnenna Kehinde <nneukamaka@yahoo.com>
2022-12-22 12:07:14 -0800

    Print Number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * largest number -returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Description: prints the largest of three integers.
+ *
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
 	int largest;
