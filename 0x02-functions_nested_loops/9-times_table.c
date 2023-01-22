ae28c668a989812819c56f0148b63d3c75d92aee
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-01-21 18:28:19 -0800

    Fixed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,13 +26,15 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			 else if (k >= 10)
-			 {
-				 _putchar(',');
-				 _putchar(' ');
-				 _putchar((k / 10) + '0');
-				 _putchar((k % 10) + '0');
-			 }
+
+			else if
+				(k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0')
+				}
 		}
 		_putchar('\n');
 	}
