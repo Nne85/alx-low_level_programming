5788cfcb0d9786a690152505814ac279b0bac2e6
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-02-02 09:35:13 -0800

    Update

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,11 +3,12 @@
 #include <stdint.h>
 
 /**
- * get_endinanness -  checks the endianness.
+ * get_endianness - checks the endianness.
  * Return:0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
 	uint32_t n = 1;
+
 	return (*(char *) &n == 1);
 }
