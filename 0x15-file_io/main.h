332ca42acec5567a5c7ff134f84b19ab96711022
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-02-09 16:01:13 -0800

    Fixed

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,22 +10,6 @@
 #include <stdio.h>
 #define ELF_MAGIC 0x464C457F
 
-typedef struct {
-unsigned char e_ident[16];
-unsigned short e_type;
-unsigned short e_machine;
-unsigned int e_version;
-unsigned int e_entry;
-unsigned int e_phoff;
-unsigned int e_shoff;
-unsigned int e_flags;
-unsigned short e_ehsize;
-unsigned short e_phentsize;
-unsigned short e_phnum;
-unsigned short e_shentsize;
-unsigned short e_shnum;
-unsigned short e_shstrndx;
-} ElfHeader;
 
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
