09f6d6d70ff3433ec6300d304e90d741b5886861
Nnenna Kehinde <nneukamaka@yahoo.com>
2023-03-17 02:17:29 -0700

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,13 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
-	
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 		count++;
 	}
-	
+
 	return (count);
 }
